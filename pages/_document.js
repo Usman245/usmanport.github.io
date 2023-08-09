e96e0e92b5696965e4ef6e4d016a49aff4f1c770
Muhammad Usman <um337757@gmail.com>
2023-08-09 13:51:33 +0500

    first deploy

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+
+      
+      </Head>
+      <body className="font-poppins">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
